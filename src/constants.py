e03ab9cacfcbc3c9d58d63b4532448b3b1dfa3d3
Astghik <asvoskerchyan@gmail.com>
2026-02-24 14:48:02 +0400

    Finish experiments for spk50-1h dataset

--- src/constants.py
+++ src/constants.py
@@ -7,15 +7,16 @@ N_MFCC = 30
 FMIN = 20.0
 FMAX = 8000.0
 
-MAX_FRAMES = 300  # fixed time length after MFCC (pad/truncate)
+MAX_FRAMES = 300
 BATCH_SIZE = 64
-EPOCHS = 10
-LEARNING_RATE = 1e-3
+EPOCHS = 30
+LEARNING_RATE = 5e-4
 
 EMB_DIM = 192
 EPS = 1e-6
 
-BEST_MODEL_PATH = "runs/spk50_1h/triplet_bh_P12K5_m0.3_e10_cnn_spk50_best.pth"
+RUNS_DIR = "runs"
+BEST_MODEL_PATH = "runs/best.pt"
 
 DATA_ROOT = r"C:\Users\User\Desktop\50spk_1h\pcm16_16k_chunks_3s"
-ALL_CHUNKS_CSV = r"C:\Users\User\Desktop\50spk_1h\all_chunks_csv"
+# ALL_CHUNKS_CSV = r"C:\Users\User\Desktop\50spk_1h\all_chunks_csv"
