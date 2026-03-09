9ad210cd2ec60dc7ef136ae94ca248bd7e324bb9
Astghik <asvoskerchyan@gmail.com>
2026-03-09 15:11:50 +0400

    adapt constants for librispeech pipeline

--- src/constants.py
+++ src/constants.py
@@ -7,7 +7,7 @@ N_MFCC = 30
 FMIN = 20.0
 FMAX = 8000.0
 
-MAX_FRAMES = 300
+MAX_FRAMES = 301
 BATCH_SIZE = 64
 EPOCHS = 30
 LEARNING_RATE = 5e-4
@@ -15,7 +15,11 @@ LEARNING_RATE = 5e-4
 EMB_DIM = 192
 EPS = 1e-6
 
+RESULTS_DIR = "results"
+
 RUNS_DIR = "runs"
 BEST_MODEL_PATH = f"{RUNS_DIR}/best.pt"
 
-DATA_ROOT = r"C:\Users\User\Desktop\data\50spk_1h\pcm16_16k_chunks_3s"
+TRAIN_ROOT = r"C:\Users\User\Desktop\Data\librispeech-train-clean-100\LibriSpeech_standardized_chunks_3s\wav\train"
+VAL_ROOT = r"C:\Users\User\Desktop\Data\librispeech-train-clean-100\LibriSpeech_standardized_chunks_3s\wav\val"
+TEST_ROOT = r"C:\Users\User\Desktop\Data\librispeech-train-clean-100\LibriSpeech_standardized_chunks_3s\wav\test"
